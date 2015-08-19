51b817be7dd708f04cd11bfa2f20a417efd1dcfd
James Bray <jrbrayjr2006@gmail.com>
2015-08-18 22:22:47 -0500

    Completely flesh out the the request mapping and parameters for the controller method to capture the survey data and pass it to the DAO

--- src/main/java/com/jaydot2/spring/SurveyController.java
+++ src/main/java/com/jaydot2/spring/SurveyController.java
@@ -6,7 +6,11 @@ package com.jaydot2.spring;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jaydot2.spring.dao.SurveyDAO;
@@ -18,6 +22,9 @@ import com.jaydot2.spring.model.Survey;
  */
 @RestController
 public class SurveyController {
+	
+	private Logger log = LogManager.getLogger(SurveyController.class);
+	
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	private SurveyDAO surveyDao;
@@ -39,11 +46,23 @@ public class SurveyController {
 		return surveyDao.getRecordCount();
 	}
 	
-	@RequestMapping("/add")
-	public int insertRecord() {
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public int insertRecord(@RequestParam(value="rating", required=true) int rating, 
+			@RequestParam(value="why_feeling", required=true) String whyFeeling,
+			@RequestParam(value="work_dissatisfaction", required=true) String workDissatisfaction,
+			@RequestParam(value="answer_matrix", required=false, defaultValue="") String answerMatrix,
+			@RequestParam(value="comments", required=false, defaultValue="no comment") String comments) {
+		log.debug("Entering insertRecord(int,String,String,String,String)...");
 		surveyDao = SurveyDAO.getInstance();
 		Survey survey = new Survey();
+		survey.setRating(rating);
+		survey.setWhyFeeling(whyFeeling);
+		survey.setWorkDissatisfaction(workDissatisfaction);
+		survey.setAnswerMatrix(answerMatrix);
+		survey.setComment(comments);
+		log.debug("Feeling is " + survey.getWhyFeeling());
 		int rows = surveyDao.insertRecord(survey);
+		log.debug("Exiting insertRecord(int,String,String,String,String)...");
 		return rows;
 	}
 }
